d2052a138ba4866bf38b1a0f9a64f3667d9d87e5
adnanali000 <adnan.museel123@gmail.com>
2022-04-03 18:33:28 +0500

    home header done and also Home screen done

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,10 +1,65 @@
-import { View, Text } from 'react-native'
+import { View, Text,Image,TextInput } from 'react-native'
 import React from 'react'
+import {SIZES,COLORS,FONTS,assets} from '../constants'
 
-const HomeHeader = () => {
+const HomeHeader = ({onSearch}) => {
+  
   return (
-    <View>
-      <Text>HomeHeader</Text>
+    <View style={{
+      backgroundColor:COLORS.primary,
+      padding:SIZES.font
+    }}>
+      <View style={{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center'
+      }}>
+        <Image 
+          source={assets.logo}
+          resizeMode='contain'
+          style={{
+            width:90,
+            height:25
+          }}
+        />
+
+        <View style={{width:45,height:45}}>
+          <Image source={assets.person01} resizeMode='contain' style={{width:'100%',height:'100%'}} />
+          <Image source={assets.badge} resizeMode='contain' style={{position:'absolute', width:15,height:15,bottom:0,right:0}} />
+
+        </View>
+      </View>
+
+      <View style={{marginVertical:SIZES.font}}>
+        <Text style={{fontSize:SIZES.small,fontFamily:FONTS.regular,color:COLORS.white}}>Hello, Adnan 👋</Text>
+
+        <Text style={{fontSize:SIZES.large,fontFamily:FONTS.bold,marginTop:SIZES.base / 2,color:COLORS.white}}>Let's find a masterpiece</Text>
+      </View>
+
+      <View style={{marginTop:SIZES.font}}>
+        <View style={{
+          width:'100%',
+          borderRadius:SIZES.font,
+          backgroundColor:COLORS.gray,
+          flexDirection:'row',
+          paddingHorizontal:SIZES.font,
+          alignItems:'center',
+          paddingVertical:SIZES.small - 2
+        }}>
+          <Image 
+            source={assets.search}
+            resizeMode="contain"
+            style={{height:20,width:20,marginRight:SIZES.base}}
+          />
+
+          <TextInput 
+            placeholder='Search NFTs' 
+            style={{flex:1,color:COLORS.white,fontSize:SIZES.medium}} 
+            onChangeText={onSearch} />
+        </View>
+
+      </View>
+
     </View>
   )
 }
